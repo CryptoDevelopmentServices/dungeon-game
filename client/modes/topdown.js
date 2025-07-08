ae8c72e8a5639391dccbcbd2fdb50ee7f491436e
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-07-09 00:30:19 +0100

    update 1
    topdown update

--- client/modes/topdown.js
+++ client/modes/topdown.js
@@ -1,23 +1,31 @@
+import { Player } from '../entities/player.js';
+
 export class TopDownScene extends Phaser.Scene {
   constructor() {
     super({ key: 'TopDownScene' });
   }
 
   preload() {
+    this.load.image('tiles', 'assets/tileset.png');
+    this.load.tilemapTiledJSON('map', 'assets/map.json');
     this.load.image('player', 'assets/player.png');
+    this.load.image('enemy', 'assets/enemy.png');
+    this.load.image('coin', 'assets/coin.png');
   }
 
   create() {
-    this.player = this.physics.add.sprite(400, 300, 'player');
-    this.cursors = this.input.keyboard.createCursorKeys();
+    const map = this.make.tilemap({ key: 'map' });
+    const tileset = map.addTilesetImage('tileset', 'tiles');
+    
+    const floorLayer = map.createLayer('Floor', tileset, 0, 0);
+    const wallLayer = map.createLayer('Walls', tileset, 0, 0);
+    wallLayer.setCollisionByProperty({ collides: true });
+
+    this.player = new Player(this, 100, 100);
+    this.physics.add.collider(this.player.sprite, wallLayer);
   }
 
   update() {
-    this.player.setVelocity(0);
-
-    if (this.cursors.left.isDown) this.player.setVelocityX(-160);
-    if (this.cursors.right.isDown) this.player.setVelocityX(160);
-    if (this.cursors.up.isDown) this.player.setVelocityY(-160);
-    if (this.cursors.down.isDown) this.player.setVelocityY(160);
+    this.player.update();
   }
 }
