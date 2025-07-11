cac05541b877c55836e4e6eb8e1305073a66c28b
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-07-12 00:10:46 +0100

    update 8

--- client/assets/tileset.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="4" columns="2">
-  <image source="tileset.png" width="64" height="64"/>
-  <tile id="0">
-    <properties>
-      <property name="collides" type="bool" value="true"/>
-    </properties>
-  </tile>
-</tileset>
