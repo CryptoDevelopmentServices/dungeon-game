06798bcf3d3ca5a0dcc3d02dcaa2b36f057fc456
Nugetzrul3 <popingcorn12@gmail.com>
2025-07-20 22:24:14 +1000

    Code migration from Main

---
+++ backend/daemon_conf.js
@@ -0,0 +1,12 @@
+export const config = {
+    endpoint: {
+        host: 'localhost',
+        port: 9982,
+        user: 'user',
+        pass: 'password',
+        rid: "dungeon-game"
+    },
+    transaction: {
+        minFee: 0.00001
+    }
+};
---
+++ backend/middleware/validationMiddleware.js
@@ -0,0 +1,10 @@
+export const validate = (schema) => async (req, res, next) => {
+    try {
+        const validateBody = await schema.validate(req.body, { abortEarly: false, stripUnknown: true });
+
+        req.body = validateBody;
+        next()
+    } catch (err) {
+        res.status(400).json({ error: err.errors[0] })
+    }
+}
